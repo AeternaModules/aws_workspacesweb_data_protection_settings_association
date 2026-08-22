9f4d3e8c5db1d96192759305c62319b21de1a5dd
Daniel Allen <dan@aeternagroup.net>
2026-08-22 23:38:23 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "workspacesweb_data_protection_settings_associations_id" {
-  description = "Map of id values across all workspacesweb_data_protection_settings_associations, keyed the same as var.workspacesweb_data_protection_settings_associations"
-  value       = { for k, v in aws_workspacesweb_data_protection_settings_association.workspacesweb_data_protection_settings_associations : k => v.id if v.id != null && length(v.id) > 0 }
-}
 output "workspacesweb_data_protection_settings_associations_data_protection_settings_arn" {
   description = "Map of data_protection_settings_arn values across all workspacesweb_data_protection_settings_associations, keyed the same as var.workspacesweb_data_protection_settings_associations"
   value       = { for k, v in aws_workspacesweb_data_protection_settings_association.workspacesweb_data_protection_settings_associations : k => v.data_protection_settings_arn if v.data_protection_settings_arn != null && length(v.data_protection_settings_arn) > 0 }
